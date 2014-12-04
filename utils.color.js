5edd0e742f33f321c7f17229ef2765fe699958ac
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2014-12-04 19:00:09 +0530

    Changed pattern to jQuery like _c(color).transform
    - _c(color) constructs a new Color
    - toHex and toRGB supported as of now

--- utils.color.js
+++ utils.color.js
@@ -1,55 +1,63 @@
-Color = function() {};
+(function() {
 
-Color.hex2rgb = function(hex) {
-    if (hex[0] == '#') {
-        hex = hex.substr(1);
-    }
-    var shortHexRe = /^[0-9a-f]{3}$/i;
-    var longHexRe = /^[0-9a-f]{6}$/i;
-    if (shortHexRe.test(hex)) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (longHexRe.test(hex)) {
-        var intRepr = parseInt(hex, 16);
-        var r = (intRepr >> 16) & 255;
-        var g = (intRepr >> 8) & 255;
-        var b = intRepr & 255;
-        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-    }
-    return null;
-}
+    var Color = (function() {
 
-Color.rgb2hex = function() {
+        var Color = function(color) {
+            return new Color.fn.init(color);
+        };
 
-    if (arguments.length == 3) {
-        var r = arguments[0];
-        var g = arguments[1];
-        var b = arguments[2];
-    }
+        Color.fn = Color.prototype = {
+            init: function(color) {
+                if (color[0] == '#') {
+                    var hex = color.substr(1);
 
-    else if (arguments.length == 1) {
-        //assuming "  rgb(r, g, b) " here
-        var rgb = arguments[0];
-        rgb = rgb.trim();
+                    var shortHexRe = /^[0-9a-f]{3}$/i;
+                    var longHexRe = /^[0-9a-f]{6}$/i;
 
-        if (rgb.substring(0, 3) == 'rgb') {
-            rgb = rgb.substr(3);
-        }
-        //(r, g, b) here
+                    if (shortHexRe.test(hex)) {
+                        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+                    }
 
-        if (rgb[0] == '(' && rgb[rgb.length - 1] == ')') {
-            rgb = rgb.substr(1).slice(0, -1);
-            var rgbArray = rgb.split(',').map(function(str) {
-                return parseInt(str.trim());
-            });
-            var r = rgbArray[0];
-            var g = rgbArray[1];
-            var b = rgbArray[2];
-            if (isNaN(r) || isNaN(g) || isNaN(b)) {
-                return null;
+                    if (longHexRe.test(hex)) {
+                        var intRepr = parseInt(hex, 16);
+                        this.r = (intRepr >> 16) & 255;
+                        this.g = (intRepr >> 8) & 255;
+                        this.b = intRepr & 255;
+                    }
+                }
+
+                else if (color.substring(0, 3) == 'rgb') {
+                    var rgb = color.substr(3);
+
+                    if (rgb[0] == '(' && rgb[rgb.length - 1] == ')') {
+                        rgb = rgb.substr(1).slice(0, -1);
+                        var rgbArray = rgb.split(',').map(function(str) {
+                            return parseInt(str.trim());
+                        });
+                        var r = rgbArray[0];
+                        var g = rgbArray[1];
+                        var b = rgbArray[2];
+                        if (!isNaN(r) && !isNaN(g) && !isNaN(b)) {
+                            this.r = r;
+                            this.g = g;
+                            this.b = b;
+                        }
+                    }
+                }
+                return this;
+            },
+            toHex: function() {
+                return "#" + ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1);
+            },
+            toRGB: function() {
+                return 'rgb(' + this.r + ', ' + this.g + ', ' + this.b + ')';
             }
         }
-    }
-    else return null;
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+
+        Color.fn.init.prototype = Color.fn;
+
+        return (window._c = window.Color = Color);
+
+    })();
+
+})();
